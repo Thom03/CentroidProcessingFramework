f4f20816aea9b4535aea2318dd80591137af80a4
Your Name <thomas.muteti@gmail.com>
2020-10-28 13:32:03 +0300

    Added Centroid Model.

--- webmapapp/app/models.py
+++ webmapapp/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.gis.db import models
+
 
 # Create your models here.
+class parcel_centroid(models.Model):
+    #  Parcel Centroid Model.
+    name = models.CharField(max_length=80)
+
+    # Geometry field(MultiPointField).
+    geom = models.MultiPointField(srid=4326)
+
+    # Returns the string representaion of the model.
+    def __str__(self):
+        return self.name
