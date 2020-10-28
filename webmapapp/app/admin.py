a9d9092053053f4cd7c26839da94991a4038dc1a
Your Name <thomas.muteti@gmail.com>
2020-10-28 13:36:01 +0300

    Registering Centroids Into Admin.

--- webmapapp/app/admin.py
+++ webmapapp/app/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
-
+from django.contrib.gis import admin
+from .models import *
 # Register your models here.
+
+# Registering centroid into Admin Page
+admin.site.register(parcel_centroid, admin.OSMGeoAdmin)
